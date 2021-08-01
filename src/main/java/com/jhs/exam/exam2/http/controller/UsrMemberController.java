417c3f975621d803955245cdcb1808465a6f72f4
Park-12 <sohyunp96@gmail.com>
2021-08-01 15:25:33 +0900

    회원가입 중, 로그인 아이디, 이름+이메일 중복체크

--- src/main/java/com/jhs/exam/exam2/http/controller/UsrMemberController.java
+++ src/main/java/com/jhs/exam/exam2/http/controller/UsrMemberController.java
@@ -73,17 +73,15 @@ public class UsrMemberController extends Controller {
 		}
 		
 		rq.debugParams();
-		
-		rq.print("여기까지 성공");
 
-		/*
 		ResultData joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 
 		if (joinRd.isFail()) {
 			rq.historyBack(joinRd.getMsg());
 			return;
 		}
-		*/
+		
+		rq.print(joinRd.toString()); // 임시
 	}
 
 	private void actionShowJoin(Rq rq) {
